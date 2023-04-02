213ffbabff81f488deb79db18d92969f21649ed7
Bléqui <gustavopregigueiro6@gmail.com>
2023-04-01 22:41:46 -0300

    Adição do código

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+decimal compra, pago, troco;
+
+Console.WriteLine("--- Troco ---");
+Console.WriteLine();
+
+Console.Write("Valor da compra (R$)...: ");
+compra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago (R$)........: ");
+pago = Convert.ToDecimal(Console.ReadLine());
+
+troco = compra - pago;
+
+Console.WriteLine();
+Console.WriteLine($"Troco: {troco:C}");
